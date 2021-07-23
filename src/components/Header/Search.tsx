8bb6de734fb00bbacce7fd9a3795eacf1075c087
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-07-23 06:02:07 +0300

    .

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {OutlinedInput} from '@material-ui/core';
 import './Header.css';
 
 const Search = (props: any) => {
+
+  const [searchedMovie, setSearchedMovie] = useState<string>('');
+  
+  useEffect(()=>{
+    console.log('Current searchedMovie ', searchedMovie);
+  }, [searchedMovie]);
+
   return (
     <>
         <OutlinedInput 
             color="secondary" 
             className="seach-field" 
-            defaultValue="Search"/>
+            type="string" 
+            onBlur={({ target: { value } }) => setSearchedMovie(value)} 
+            placeholder="Expense cost" 
+            value={searchedMovie}
+            />
     </>
   );
 }
 
 export default Search;
+/* 
+
+ onChange={({ target: { value } }) => onChange(value)} 
+
+*/
+
+
+/*
+            <InputItem 
+                onChange={setAmount}  
+                onBlur={setAmount}  
+                title="Amount" 
+                type="number" 
+                placeholder="Expense cost" 
+                value={Amount}
+            
+            />
+*/
