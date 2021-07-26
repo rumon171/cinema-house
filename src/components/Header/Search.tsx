594884de53f9b3fd87ccf092fff6e0d13665c868
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-07-26 19:54:23 +0300

    .

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -9,28 +9,22 @@ const Search = (props: any) => {
   const { updateMovies } = useContext(MoviesContext); 
 
   const [insertedTitle, setInsertedTitle] = useState<string>('');
-
-  const handleKeyPress = (e: any) => {
-    if (e.keyCode === 13) {
-      if (insertedTitle) {
-        fetchSearchedMovie(insertedTitle)
-          .then((res) => updateMovies(res))
-          .catch(() => updateMovies([]));
-      } else {
-        fetchMovies()
-          .then((res) => updateMovies(res))
-          .catch(() => updateMovies([]));
-      }
+  
+  const fetchMoviesList = () => {
+    if (insertedTitle) {
+      fetchSearchedMovie(insertedTitle)
+      .then((res) => updateMovies(res))
+      .catch(() => updateMovies([]));
+    } else {
+      fetchMovies()
+      .then((res) => updateMovies(res))
+      .catch(() => updateMovies([]));
     }
   }
 
-  const fetchByEnteredTitle = () => {
-    if (insertedTitle) {
-      fetchSearchedMovie(insertedTitle)
-        .then((res) => updateMovies(res))
-        .catch(() => updateMovies([]));
-    } else {
-      console.log("HER2 ");
+  const handleKeyPress = (e: any) => {
+    if (e.keyCode === 13) {
+      fetchMoviesList();
     }
   }
  
@@ -41,7 +35,7 @@ const Search = (props: any) => {
           className="seach-field" 
           type="string" 
           onChange={({ target: { value } }) => setInsertedTitle(value)} 
-          onBlur={fetchByEnteredTitle} 
+          onBlur={fetchMoviesList} 
           onKeyDown={ (e) => handleKeyPress(e) }
           placeholder="Search" 
           defaultValue={insertedTitle}
