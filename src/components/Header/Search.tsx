1ac2dc02e2024a41fd500e26291def292a9d28d2
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-07-26 19:48:03 +0300

    fetch on entering no value

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import {OutlinedInput} from '@material-ui/core';
 import './Header.css';
 import { MoviesContext } from "../../services/context";
-import { fetchSearchedMovie } from "../../services/movies.service";
+import { fetchSearchedMovie, fetchMovies } from "../../services/movies.service";
 
 const Search = (props: any) => {
 
@@ -16,6 +16,10 @@ const Search = (props: any) => {
         fetchSearchedMovie(insertedTitle)
           .then((res) => updateMovies(res))
           .catch(() => updateMovies([]));
+      } else {
+        fetchMovies()
+          .then((res) => updateMovies(res))
+          .catch(() => updateMovies([]));
       }
     }
   }
@@ -25,21 +29,23 @@ const Search = (props: any) => {
       fetchSearchedMovie(insertedTitle)
         .then((res) => updateMovies(res))
         .catch(() => updateMovies([]));
+    } else {
+      console.log("HER2 ");
     }
   }
  
   return (
     <>
-        <OutlinedInput 
-            color="secondary" 
-            className="seach-field" 
-            type="string" 
-            onChange={({ target: { value } }) => setInsertedTitle(value)} 
-            onBlur={fetchByEnteredTitle} 
-            onKeyDown={ (e) => handleKeyPress(e) }
-            placeholder="Search" 
-            defaultValue={insertedTitle}
-            />
+      <OutlinedInput 
+          color="secondary" 
+          className="seach-field" 
+          type="string" 
+          onChange={({ target: { value } }) => setInsertedTitle(value)} 
+          onBlur={fetchByEnteredTitle} 
+          onKeyDown={ (e) => handleKeyPress(e) }
+          placeholder="Search" 
+          defaultValue={insertedTitle}
+          />
     </>
   );
 }
