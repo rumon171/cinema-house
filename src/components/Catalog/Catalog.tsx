850999d5bd28d5ece474ca586c0ee6f30227fd7f
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-08-10 19:33:42 +0300

    .

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,9 +1,21 @@
+import{ useEffect } from "react";
 import CatalogCards from './CatalogCards';
 import ScrollTop from '../Catalog/ScrollTop';
 import { Toolbar, Fab } from '@material-ui/core';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
 const Catalog = (props: any) => {
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  function handleScroll() {
+    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+    console.log('Fetch more list items!');
+  }
+
   return (
     <>
         <Toolbar id="back-to-top-anchor" />
