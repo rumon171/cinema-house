080311c4b0472680b5a25b59616f30100983d79b
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-08-15 11:58:10 +0300

    .

--- src/components/Catalog/CatalogCards.tsx
+++ src/components/Catalog/CatalogCards.tsx
@@ -1,4 +1,4 @@
-import{ useContext, useRef } from "react";
+import{ useContext, useRef, useEffect } from "react";
 import { Card, Grid, CardActionArea, CardActions, CardMedia, Button } from '@material-ui/core';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import { MoviesContext } from "../../services/context";
@@ -23,7 +23,12 @@ const CatalogCards = () =>  {
     setSelectedMovie(id);
   }
 
-console.log(`LOADING REF VISIBLE`, { isVisible });
+useEffect (
+  () => {
+    console.log(`LOADING REF VISIBLE`, { isVisible });
+  },
+  [isVisible]
+);
 
   return (
     <div >
@@ -72,3 +77,52 @@ console.log(`LOADING REF VISIBLE`, { isVisible });
 }
 
 export default CatalogCards;
+/*
+  return (
+    <div >
+      <Grid container spacing={1} className="container-content">
+        { 
+        movies.length > 0 
+          ? 
+          movies.map((movie, i) => (
+            <Grid item xs={12} sm={6} md={3} lg={2} key={movie.id}>
+              <NavLink to={'/movie/' + movie.id}>
+                <Card className="card-list" onClick={() => SetSelectedMovieId(movie.id)} >
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      alt={"Poster of " + movie.title}
+                      image={movie.poster_path ? posterBaseUrl + movie.poster_path : noImage}
+                      title={movie.title}
+                    />
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      <FavoriteBorderIcon />
+                    </Button>
+                    <Button size="small" color="primary">
+                      {movie.vote_average}
+                    </Button>
+                  </CardActions>
+                </Card>
+              </NavLink>
+            </Grid>
+          ))
+          :
+            searchedMovie ?
+            <div className="">Try a different phrase...</div>
+            :
+            <CardMedia
+            component="img"
+            image={loadingSpinner}
+            className="loading-spinner"
+          />
+        }
+      </Grid>
+      <div ref={loadingRef}>...</div>
+    </div>
+  );
+}
+
+export default CatalogCards;
+*/
