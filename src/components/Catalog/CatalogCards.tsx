86d62101b3c91fcc9b16abe06a8703987841dd37
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-08-15 12:43:09 +0300

    .

--- src/components/Catalog/CatalogCards.tsx
+++ src/components/Catalog/CatalogCards.tsx
@@ -2,17 +2,19 @@ import{ useContext, useRef, useEffect } from "react";
 import { Card, Grid, CardActionArea, CardActions, CardMedia, Button } from '@material-ui/core';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import { MoviesContext } from "../../services/context";
+import { Movie } from "../../services/movies.service";
 import '../../App.scss';
 import './Catalog.scss';
 import noImage from '../../images/no-image-available.png';
 import loadingSpinner from '../../images/loading-spinner.gif';
 import { NavLink } from 'react-router-dom';
 import useIntersectionObserver from '../../services/useIntersectionObserver';
+import { fetchMovies } from "../../services/movies.service";
 
 const posterBaseUrl = "https://image.tmdb.org/t/p/w300";
 
 const CatalogCards = () =>  {
-  const { movies, searchedMovie, moviesPage, setMoviesPage, setSelectedMovie, setIsMoviePageFirstTimeOpened } = useContext(MoviesContext);
+  const { movies, updateMovies, searchedMovie, moviesPage, setMoviesPage, setSelectedMovie, setIsMoviePageFirstTimeOpened } = useContext(MoviesContext);
   const loadingRef = useRef<HTMLDivElement | null>(null);
   const entry = useIntersectionObserver(loadingRef, {})
   const isVisible = !!entry?.isIntersecting;
@@ -22,15 +24,22 @@ const CatalogCards = () =>  {
     setSelectedMovie(id);
   }
 
-useEffect (
-  () => {
-    console.log(`LOADING REF VISIBLE`, { isVisible }, " page ", moviesPage);
-    if ( isVisible ) {
-      setMoviesPage(moviesPage+1);
-    }
-  },
-  [isVisible]
-);
+  useEffect (
+    () => {
+      console.log(`LOADING REF VISIBLE`, { isVisible }, " page ", moviesPage);
+      if ( isVisible ) {
+        //setMoviesPage(moviesPage+1);
+
+        fetchMovies(String(moviesPage+1))
+          .then(nextPage => {
+            updateMovies(movies => movies.concat(nextPage));
+          })
+          .catch(() => updateMovies([]))
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [isVisible]
+  );
 
   return (
     <div >
@@ -79,52 +88,3 @@ useEffect (
 }
 
 export default CatalogCards;
-/*
-  return (
-    <div >
-      <Grid container spacing={1} className="container-content">
-        { 
-        movies.length > 0 
-          ? 
-          movies.map((movie, i) => (
-            <Grid item xs={12} sm={6} md={3} lg={2} key={movie.id}>
-              <NavLink to={'/movie/' + movie.id}>
-                <Card className="card-list" onClick={() => SetSelectedMovieId(movie.id)} >
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      alt={"Poster of " + movie.title}
-                      image={movie.poster_path ? posterBaseUrl + movie.poster_path : noImage}
-                      title={movie.title}
-                    />
-                  </CardActionArea>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      <FavoriteBorderIcon />
-                    </Button>
-                    <Button size="small" color="primary">
-                      {movie.vote_average}
-                    </Button>
-                  </CardActions>
-                </Card>
-              </NavLink>
-            </Grid>
-          ))
-          :
-            searchedMovie ?
-            <div className="">Try a different phrase...</div>
-            :
-            <CardMedia
-            component="img"
-            image={loadingSpinner}
-            className="loading-spinner"
-          />
-        }
-      </Grid>
-      <div ref={loadingRef}>...</div>
-    </div>
-  );
-}
-
-export default CatalogCards;
-*/
