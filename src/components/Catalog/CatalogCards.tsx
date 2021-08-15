26196a0a699ede97692ee02490a73e3c04949e20
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-08-15 09:38:57 +0300

    .

--- src/components/Catalog/CatalogCards.tsx
+++ src/components/Catalog/CatalogCards.tsx
@@ -7,33 +7,40 @@ import './Catalog.scss';
 import noImage from '../../images/no-image-available.png';
 import loadingSpinner from '../../images/loading-spinner.gif';
 import { NavLink } from 'react-router-dom';
-import 'intersection-observer';
-import 'intersection-observer/intersection-observer.js'
+//import 'intersection-observer';
+//import 'intersection-observer/intersection-observer.js'
+import useIntersectionObserver from '../../services/useIntersectionObserver';
 
 const posterBaseUrl = "https://image.tmdb.org/t/p/w300";
 
 const CatalogCards = () =>  {
   const { movies, searchedMovie } = useContext(MoviesContext);  
   const { setSelectedMovie, setIsMoviePageFirstTimeOpened } = useContext(MoviesContext);
-  const loadingRef = useRef<HTMLDivElement|null>(null);
+  const loadingRef = useRef<HTMLDivElement | null>(null);
+  const entry = useIntersectionObserver(loadingRef, {})
+  const isVisible = !!entry?.isIntersecting;
 
   const SetSelectedMovieId = (id: number) => {
     setIsMoviePageFirstTimeOpened(true);
     setSelectedMovie(id);
   }
   
+  /*
   const isBottomVisible = useIntersectionObserver(
     loadingRef,
     {
-      threshold: 0 //trigger event as soon as the element is in the viewport.
+      threshold: 0 // to trigger event as soon as the element is in the viewport
     },
-    false // don't remove the observer after intersected.
+    false // to not remove the observer after intersected
   );
 
   useEffect(() => {
     //load next page when bottom is visible
     isBottomVisible && console.log("BOTTOM REACHED");
   }, [isBottomVisible]);
+*/
+
+console.log(`LOADING REF VISIBLE`, { isVisible });
 
   return (
     <div >
