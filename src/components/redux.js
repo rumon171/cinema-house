7ae1799dbec6eda80d645a43c31004995cf9e9e9
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-08-17 07:36:36 +0300

    .

---
+++ src/components/redux.js
@@ -0,0 +1,9 @@
+export const SET_SELECTED_MOVIE = 'SET_SELECTED_MOVIE' // action types
+
+export function setselectedMovie(selectedMovie) {
+  return
+     {     
+        SET_SELECTED_MOVIE,
+        selectedMovie     // action payload
+     }
+}
