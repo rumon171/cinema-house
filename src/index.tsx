c184122677d119cd43bb61c55ce7837d7841aed1
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-08-18 05:45:21 +0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
+import store from "./store";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
