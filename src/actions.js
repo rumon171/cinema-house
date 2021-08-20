38061568c02d6075c96e0007c8c59c5265c90492
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-08-20 17:09:33 +0300

    .

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,17 @@
 export const CHANGE_SELECTED_MOVIE = 'CHANGE_SELECTED_MOVIE' // action types
+export const IS_MOVIE_PAGE_FIRST_TIME_OPENED = 'IS_MOVIE_PAGE_FIRST_TIME_OPENED' // action types
 
+// MAYBE DO AS PER EXAMPLE BELOW?
 export function changeSelectedMovie( selectedMovie ) {
    return { 
       type: 'CHANGE_SELECTED_MOVIE', 
       payload: selectedMovie 
    }
  }
+
+ export function itemsRequestSuccess( boolean ) {
+   return {
+      type: IS_MOVIE_PAGE_FIRST_TIME_OPENED,
+      isLoading: boolean,
+   }
+}
