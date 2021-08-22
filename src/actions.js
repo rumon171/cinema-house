5513009dbe029bf4202ce58957924a35cb2774bf
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-08-22 08:08:51 +0300

    .

--- src/actions.js
+++ src/actions.js
@@ -1,14 +1,14 @@
 export const CHANGE_SELECTED_MOVIE = 'CHANGE_SELECTED_MOVIE' // action types
 export const IS_MOVIE_PAGE_FIRST_TIME_OPENED = 'IS_MOVIE_PAGE_FIRST_TIME_OPENED' // action types
 
-export function changeSelectedMovie( id ) {
+export function changeSelectedMovie ( id ) {
    return { 
       type: 'CHANGE_SELECTED_MOVIE', 
       selectedMovie: id
    }
  }
 
- export function itemsRequestSuccess( boolean ) {
+ export function itemsRequestSuccess ( boolean ) {
    return {
       type: IS_MOVIE_PAGE_FIRST_TIME_OPENED,
       isLoading: boolean,
