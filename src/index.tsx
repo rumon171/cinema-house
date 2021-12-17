6a073d9140b635c7e4d0a1e3d2235754ba10c319
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-12-17 11:37:53 +0200

    report web vitals removed

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
-import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,5 +11,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
-reportWebVitals()
