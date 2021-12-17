0a982c4f15e39496414aaacfb9a950ff880b9b2f
Ruta <rutakalytyte@Ruta-Kalytyte-MacBook-Pro.local>
2021-12-17 08:36:19 +0200

    TS types added to Redux actions

--- src/actions.ts
+++ src/actions.ts
@@ -1,40 +1,42 @@
+import { Movie } from "./services/movies.service";
+
 export const ADD_HOME_PAGE_MOVIES = 'ADD_HOME_PAGE_MOVIES'
 export const CHANGE_SELECTED_MOVIE = 'CHANGE_SELECTED_MOVIE'
 export const IS_MOVIE_PAGE_OPENED = 'IS_MOVIE_PAGE_OPENED'
 export const SEARCHED_MOVIE = 'SEARCHED_MOVIE'
 export const CURRENT_PAGE = 'CURRENT_PAGE'
 
-export function showMoviesAtHomePage ( additionalMovies ) {
+export function showMoviesAtHomePage ( additionalMovies: Movie[] ): {type: string; homePageMovies: Movie[]} {
    return { 
       type: 'ADD_HOME_PAGE_MOVIES', 
       homePageMovies: additionalMovies
    }
  }
 
- export function changeSelectedMovie ( id ) {
+ export function changeSelectedMovie ( id: number ): {type: string; selectedMovie: number} {
    return { 
       type: 'CHANGE_SELECTED_MOVIE', 
       selectedMovie: id
    }
  }
 
- export function isMoviePageOpened ( boolean ) {
+ export function isMoviePageOpened ( isMovieOpened: boolean ): {type: string; isMoviePageOpened: boolean} {
    return {
       type: IS_MOVIE_PAGE_OPENED,
-      isMoviePageOpened: boolean
+      isMoviePageOpened: isMovieOpened
    }
 }
 
-export function changeSearchedMovie ( phrase ) {
+export function changeSearchedMovie ( phrase: string ): {type: string; searchedMovie: string} {
    return {
       type: SEARCHED_MOVIE,
       searchedMovie: phrase
    }
 }
 
-export function changeCurrentPage ( number ) {
+export function changeCurrentPage ( currentPage: number ): {type: string; currentPage: number} {
    return {
       type: CURRENT_PAGE,
-      currentPage: number
+      currentPage: currentPage
    }
 }
