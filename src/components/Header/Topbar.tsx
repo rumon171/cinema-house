6d1581fde9ac74871e28dfed2497d47195ac3647
ruta2 <ruta.kalytyte@revelsystems.com>
2022-01-28 14:56:15 +0200

    .

--- src/components/Header/Topbar.tsx
+++ src/components/Header/Topbar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AppBar, Toolbar } from '@material-ui/core';
-import './Header.scss';
 import Search from './Search';
 import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
@@ -9,30 +8,30 @@ import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined'
 import useStyles from './Topbar.styles';
 
 const Topbar: React.FC = () => {
-  const dispatch = useDispatch();
-  const classes = useStyles();
+    const dispatch = useDispatch();
+    const classes = useStyles();
 
-  const handleHomePageLink = () => {
-    dispatch(changeSearchedMovie(''));
-    dispatch(isMoviePageOpened(false));
-    window.scrollTo(0, 0);
-  };
+    const handleHomePageLink = () => {
+        dispatch(changeSearchedMovie(''));
+        dispatch(isMoviePageOpened(false));
+        window.scrollTo(0, 0);
+    };
 
-  return (
-    <>
-      <AppBar>
-        <Toolbar className={classes.topBar}>
-          <NavLink to={'/'} onClick={handleHomePageLink}>
-            <h1 className={classes.topBarTitle}>Cinema House</h1>
-          </NavLink>
-          <Search />
-          <NavLink to={'/profile'}>
-            <PersonOutlineOutlinedIcon />
-          </NavLink>
-        </Toolbar>
-      </AppBar>
-    </>
-  );
+    return (
+        <>
+            <AppBar>
+                <Toolbar className={classes.topBar}>
+                    <NavLink to={'/'} onClick={handleHomePageLink}>
+                        <h1 className={classes.topBarTitle}>Cinema House</h1>
+                    </NavLink>
+                    <Search />
+                    <NavLink to={'/profile'}>
+                        <PersonOutlineOutlinedIcon />
+                    </NavLink>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
 };
 
 export default Topbar;
