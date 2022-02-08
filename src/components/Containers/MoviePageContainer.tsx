67a4d5d47e388d074e6965312aded28563f011b7
jazzmam <cantaccessmy@gmail.com>
2022-02-08 08:22:17 +0200

    .

--- src/components/Containers/MoviePageContainer.tsx
+++ src/components/Containers/MoviePageContainer.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Topbar from '../Header/Topbar';
-import './Containers';
+import './Containers.scss';
 import MoviePage from '../MoviePage/MoviePage';
 // import { useNavigate } from 'react-router-dom';
 // import { useDispatch } from 'react-redux';
