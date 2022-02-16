c84b756a671bca3863d330b5d4e3daaadbfd7ecb
jazzmam <cantaccessmy@gmail.com>
2022-02-16 16:49:49 +0200

    CH-1-1: header menu css

--- src/components/Header/HeaderMenu.styles.ts
+++ src/components/Header/HeaderMenu.styles.ts
@@ -2,10 +2,19 @@ import { makeStyles } from '@material-ui/core';
 
 export default makeStyles(() => ({
     container: {
-        backgroundColor: '#333442',
-        //272838
-        marginTop: '70px',
-        marginBottom: '-70px',
+        backgroundColor: '#414252',
+        paddingTop: '70px',
+        paddingBottom: '20px',
+        marginBottom: '-30px',
+        ['@media (min-width:301px)']: {
+            paddingTop: '90px',
+        },
+        ['@media (min-width:391px)']: {
+            paddingTop: '70px',
+        },
+        ['@media (min-width:601px)']: {
+            paddingTop: '80px',
+        },
     },
     filterButton: {
         backgroundColor: '#20c689',
--- src/components/Header/HeaderTopbar.styles.ts
+++ src/components/Header/HeaderTopbar.styles.ts
@@ -3,14 +3,30 @@ import { makeStyles } from '@material-ui/core';
 export default makeStyles(theme => ({
     topBar: {
         backgroundColor: theme.palette.primary.main,
+        [theme.breakpoints.up('xs')]: {
+            paddingLeft: '10px',
+            paddingRight: '10px',
+        },
+        ['@media (min-width:300px)']: {
+            paddingLeft: '16px',
+            paddingRight: '16px',
+        },
         '& a': {
             textDecoration: 'none',
             color: theme.palette.common.white,
         },
     },
     topBarTitle: {
-        fontSize: '1.5rem',
         fontWeight: 500,
+        [theme.breakpoints.up('xs')]: {
+            fontSize: '0.8rem',
+        },
+        ['@media (min-width:300px)']: {
+            fontSize: '1.1rem',
+        },
+        [theme.breakpoints.up('sm')]: {
+            fontSize: '1.5rem',
+        },
     },
     searchField: {
         marginLeft: 'auto',
